5ce343316e797acb17bb3a68de1ef9e295494e11
Mike Schrijver <mike.schrijver@topicus.nl>
2025-04-18 15:58:07 +0200

    Ex 1. Start 2

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,24 +1,21 @@
-import React from 'react';
+import BlogHero from "@/components/BlogHero";
 
-import BlogHero from '@/components/BlogHero';
+import { loadBlogPost } from "@/helpers/file-helpers";
+import { MDXRemote } from "next-mdx-remote/rsc";
+import styles from "./postSlug.module.css";
 
-import styles from './postSlug.module.css';
+async function BlogPost({ params }) {
+  const { postSlug } = await params;
 
-function BlogPost() {
+  const { frontmatter, content } = await loadBlogPost(postSlug);
+
   return (
     <article className={styles.wrapper}>
       <BlogHero
-        title="Example post!"
-        publishedOn={new Date()}
+        title={frontmatter.title}
+        publishedOn={frontmatter.publishedOn}
       />
-      <div className={styles.page}>
-        <p>This is where the blog post will go!</p>
-        <p>
-          You will need to use <em>MDX</em> to render all of
-          the elements created from the blog post in this
-          spot.
-        </p>
-      </div>
+      <div className={styles.page}>{<MDXRemote source={content} />}</div>
     </article>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React from "react";
 
-import BlogSummaryCard from '@/components/BlogSummaryCard';
+import BlogSummaryCard from "@/components/BlogSummaryCard";
 
-import styles from './homepage.module.css';
+import styles from "./homepage.module.css";
+import { getBlogPostList } from "@/helpers/file-helpers";
 
-function Home() {
+async function Home() {
   return (
     <div className={styles.wrapper}>
-      <h1 className={styles.mainHeading}>
-        Latest Content:
-      </h1>
-
-      {/* TODO: Iterate over the data read from the file system! */}
-      <BlogSummaryCard
-        slug="example"
-        title="Hello world!"
-        abstract="This is a placeholder, an example which shows how the “BlogSummaryCard” component should be used. You'll want to swap this out based on the data from the various MDX files!"
-        publishedOn={new Date()}
-      />
+      <h1 className={styles.mainHeading}>Latest Content:</h1>
+      <div className={styles.blogList}>
+        {await getBlogPostList().then((posts) =>
+          posts.map((post) => (
+            <BlogSummaryCard
+              key={post.slug}
+              title={post.title}
+              description={post.description}
+              publishedOn={post.publishedOn}
+              slug={post.slug}
+            />
+          ))
+        )}
+      </div>
     </div>
   );
 }
