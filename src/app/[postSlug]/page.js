123b3d81c06b6cadb73c0c2ba19d5fb43188d858
Mike Schrijver <mike.schrijver@topicus.nl>
2025-05-08 10:29:52 +0200

    X

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -3,6 +3,22 @@ import BlogHero from "@/components/BlogHero";
 import { loadBlogPost } from "@/helpers/file-helpers";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import styles from "./postSlug.module.css";
+import { BLOG_TITLE } from "@/constants";
+import CodeSnippet from "@/components/CodeSnippet";
+
+import DivisionGroupsDemo from "@/components/DivisionGroupsDemo";
+import COMPONENT_MAP from "@/helpers/mdx-components";
+
+export async function generateMetadata({ params }) {
+  const { postSlug } = params;
+
+  const { frontmatter } = await loadBlogPost(postSlug);
+
+  return {
+    title: `${frontmatter.title} | ${BLOG_TITLE}`,
+    description: frontmatter.abstract,
+  };
+}
 
 async function BlogPost({ params }) {
   const { postSlug } = await params;
@@ -15,7 +31,9 @@ async function BlogPost({ params }) {
         title={frontmatter.title}
         publishedOn={frontmatter.publishedOn}
       />
-      <div className={styles.page}>{<MDXRemote source={content} />}</div>
+      <div className={styles.page}>
+        <MDXRemote source={content} components={COMPONENT_MAP} />
+      </div>
     </article>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,40 @@
-import React from 'react';
-import clsx from 'clsx';
-import { Rss, Sun, Moon } from 'react-feather';
+"use client";
 
-import Logo from '@/components/Logo';
-import VisuallyHidden from '@/components/VisuallyHidden';
+import clsx from "clsx";
+import { useState } from "react";
+import { Moon, Rss, Sun } from "react-feather";
+import Cookie from "js-cookie";
 
-import styles from './Header.module.css';
+import Logo from "@/components/Logo";
+import VisuallyHidden from "@/components/VisuallyHidden";
 
-function Header({ theme, className, ...delegated }) {
+import styles from "./Header.module.css";
+import {
+  COLOR_THEME_COOKIE_NAME,
+  DARK_TOKENS,
+  LIGHT_TOKENS,
+} from "@/constants";
+
+function Header({ initialTheme, className, ...delegated }) {
+  const [theme, setTheme] = useState(initialTheme);
+
+  function handleToggleTheme() {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+
+    Cookie.set(COLOR_THEME_COOKIE_NAME, newTheme, { expires: 1000 });
+
+    const newTokens = newTheme === "light" ? LIGHT_TOKENS : DARK_TOKENS;
+    const root = document.documentElement;
+
+    Object.entries(newTokens).forEach(([key, value]) => {
+      root.style.setProperty(key, value);
+    });
+    root.setAttribute("data-color-theme", newTheme);
+  }
+
   return (
-    <header
-      className={clsx(styles.wrapper, className)}
-      {...delegated}
-    >
+    <header className={clsx(styles.wrapper, className)} {...delegated}>
       <Logo />
 
       <div className={styles.actions}>
@@ -21,18 +43,14 @@ function Header({ theme, className, ...delegated }) {
             size="1.5rem"
             style={{
               // Optical alignment
-              transform: 'translate(2px, -2px)',
+              transform: "translate(2px, -2px)",
             }}
           />
-          <VisuallyHidden>
-            View RSS feed
-          </VisuallyHidden>
+          <VisuallyHidden>View RSS feed</VisuallyHidden>
         </button>
-        <button className={styles.action}>
-          <Sun size="1.5rem" />
-          <VisuallyHidden>
-            Toggle dark / light mode
-          </VisuallyHidden>
+        <button className={styles.action} onClick={handleToggleTheme}>
+          {theme === "light" ? <Moon size="1.5rem" /> : <Sun size="1.5rem" />}
+          <VisuallyHidden>Toggle dark / light mode</VisuallyHidden>
         </button>
       </div>
     </header>
